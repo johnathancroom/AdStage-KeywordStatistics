7d9e108222f9a3c724521b75ec140ae93dabcec1
Johnathan Croom <johnathancroom@gmail.com>
2012-10-06 20:35:05 -0400

    Finally connected to AdWords API
    - Depends on config file which isn't yet committed

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -1,3 +1,5 @@
+require 'adwords_api'
+
 class KeywordsController < ApplicationController
   def create
     # Convert submitted array of keywords to an Array
@@ -9,8 +11,14 @@ class KeywordsController < ApplicationController
     # Remove empty keywords
     keywords = keywords.reject(&:empty?)
 
+    # AdwordsApi is going to look for config file in ENV["HOME"]
+    ENV["HOME"] = Rails.root.join("config").to_s
+    adwords = AdwordsApi::Api.new
+    tis = adwords.service(:CampaignService, :v201209)
+    stuff = tis.get({:fields => ["Id", "Name"]})
+
     # Render keywords in JSON format
-    render :json => keywords
+    render :text => stuff.inspect
 
     # TODO: Fallback if form was actually submitted (non-ajax)
   end
